111ef8a8a2e283b255eb6a14b06aea00c91f458e
AKULALOK <159147021+AKULALOK@users.noreply.github.com>
2024-02-07 08:55:54 +0000

    conversionoperations

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,16 @@
+let isLoggedIn = ""
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let score = "33"
+
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(typeof (valueInNumber))
+
+//"33" => 33
+//"33abc"=> Nan
+//true => 1,false => 0
+
+//1=> true, string => true. 
+// "" => false, 0 => false.
