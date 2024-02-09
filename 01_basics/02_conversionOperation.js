773fdf6caafa7d95e6e10f360efab05de18e8897
AKULALOK <159147021+AKULALOK@users.noreply.github.com>
2024-02-09 07:08:18 +0000

    Dates

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -14,3 +14,16 @@ console.log(typeof (valueInNumber))
 
 //1=> true, string => true. 
 // "" => false, 0 => false.
+
+//--------------------------------------------------------------------
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(a, b);
+// Expected output: "a:4, b:4"
---
+++ 01_basics/Date.js
@@ -0,0 +1,9 @@
+let myDate =new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// //console.log(myDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp)
+console.log(myDate.getTime());
